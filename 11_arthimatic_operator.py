b228e24aed1f48893c60ba71a9b4b16fffa8db67
theeasylearn <theeasylearn@gmail.com>
2025-08-05 18:42:05 +0530

    arthimatic_operator

---
+++ 11_arthimatic_operator.py
@@ -0,0 +1,31 @@
+# arthimatic operators in python 
+# binary operators
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+
+#addition (expression/process) samikaran 
+addition = num1 + num2 
+print(f"addition = {addition}")
+#subtraction
+sub = num1 - num2 
+print(f"subtraction = {sub}")
+
+#multiplication 
+mul = num1 * num2 
+print(f"multiplication = {mul}")
+
+#division
+div = num1 / num2 
+print(f"division = {div}")
+
+#floor division 
+div2 = num1 // num2 
+print(f"floor division = {div2}")
+
+#modules operation
+reminder = num1 % num2 
+print(f"modulas operation = {reminder}")
+
+#exponent 
+power = num1 ** num2 
+print(f"power = {power}")
