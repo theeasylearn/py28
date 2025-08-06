dce18a75dd89757456606c2f53dbdf68216bd0eb
theeasylearn <theeasylearn@gmail.com>
2025-08-06 18:45:19 +0530

    operators

---
+++ 12_relational_operator.py
@@ -0,0 +1,21 @@
+#relational operator in python 
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+result = num1<num2
+print(f"{result} = {num1} < {num2}")
+
+result = num1>num2
+print(f"{result} = {num1} > {num2}")
+
+result = num1<=num2
+print(f"{result} = {num1} <= {num2}")
+
+result = num1>=num2
+print(f"{result} = {num1} >= {num2}")
+
+result = num1==num2
+print(f"{result} = {num1} == {num2}")
+
+result = num1!=num2
+print(f"{result} = {num1} != {num2}")
