e7441d27189fc063ce1a0507853bbe7be0f6992e
theeasylearn <theeasylearn@gmail.com>
2025-08-22 19:01:15 +0530

    loop

---
+++ 24_loop_1.py
@@ -0,0 +1,11 @@
+# write a program to Print 1 2 3 4 5 6 7 8 9 10 … 10000
+# wrong way
+# print(1)
+# print(2)
+# print(3)
+# check each output
+num = 1
+while num<=10000:
+    print(num,end=' ')
+    num = num + 1 
+
