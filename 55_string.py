def523785081dafa7735d3e82306896e0bc72f3c
Dpatelit <dpatel99.it@gmail.com>
2025-09-09 17:01:31 +0530

    string

---
+++ 55_string.py
@@ -0,0 +1,7 @@
+s = " We are learning Python at the easy learn"
+print(f"string in lower {s.lower()}")
+print(f"string in upper {s.upper()}")
+print(f"string is in title {s.title()}")
+print(f"length of string is {len(s)}")
+print(f"remove whitespace from string {s.strip()}")
+
---
+++ 56_join.py
@@ -0,0 +1,9 @@
+a = ['usa','uk','england','india']
+
+s = ",".join(a)
+print(s)
+s1 = "India is a country where India’s diversity, India’s culture, India’s history, and India’s unity are celebrated."
+print(s1)
+print(f"India =Bharat {s1.replace('India','Bharat')}")
+print(f"India =Bharat {s1.replace('India','Bharat',1)}")
+print(f"split of s {s.split(',')}")
