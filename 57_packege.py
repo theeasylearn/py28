36ff7df69f45277bcdac3ad0a9cfe02e6e86930b
Dpatelit <dpatel99.it@gmail.com>
2025-09-11 16:36:09 +0530

    module

---
+++ 57_packege.py
@@ -0,0 +1,10 @@
+import world.continents as wc
+import world.africa.africa as af
+from world.asia import asia as ac
+
+print()
+wc.getcontinents()
+print()
+af.getafrican()
+print()
+ac.getasia()
---
+++ world/continents.py
@@ -0,0 +1,12 @@
+continents = [
+    "Africa",
+    "Antarctica",
+    "Asia",
+    "Europe",
+    "North America",
+    "Australia",
+    "South America"
+]
+def getcontinents():
+    print("continents of world",continents)
+
