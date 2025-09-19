c8f8ed7a1f04353cb924443ce0de0123ea8d1825
Dpatelit <dpatel99.it@gmail.com>
2025-09-19 16:56:10 +0530

    class

---
+++ 67_class.py
@@ -0,0 +1,18 @@
+class Animal: 
+	# A sample method 
+	def eat(self): 
+		print("I can eat") 
+	def run(self): 
+		print("I can run") 
+	def sleep(self):
+		print("I can sleep")
+#create object
+#object = classname()
+Tiger = Animal() #creating object of animal class 
+Tiger.eat()
+Tiger.run()
+Tiger.sleep()
+Elephant = Animal()
+Elephant.eat()
+Elephant.sleep()
+Elephant.run()
